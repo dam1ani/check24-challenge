60873a0136f69bb23d54aad76f854ec40352d83e
dam1ani <damianpaluca@outlook.com>
2022-11-13 23:04:45 +0100

    Add airpots

---
+++ src/frontend/components/constants.ts
@@ -0,0 +1,182 @@
+export const AIRPORTS = [
+  {
+    "code": "LEJ",
+    "label": "Leipzig"
+  },
+  {
+    "code": "STR",
+    "label": "Stuttgart"
+  },
+  {
+    "code": "HAM",
+    "label": "Hamburg"
+  },
+  {
+    "code": "DUS",
+    "label": "Duesseldorf"
+  },
+  {
+    "code": "DRS",
+    "label": "Dresden"
+  },
+  {
+    "code": "FRA",
+    "label": "Frankfurt"
+  },
+  {
+    "code": "CGN",
+    "label": "Cologne"
+  },
+  {
+    "code": "MUC",
+    "label": "Munich"
+  },
+  {
+    "code": "FMO",
+    "label": "Munster"
+  },
+  {
+    "code": "SCN",
+    "label": "Saarbruecken"
+  },
+  {
+    "code": "DTM",
+    "label": "Dortmund"
+  },
+  {
+    "code": "NUE",
+    "label": "Nuernberg"
+  },
+  {
+    "code": "FKB",
+    "label": "Karlsruhe/Baden-Baden"
+  },
+  {
+    "code": "HAJ",
+    "label": "Hannover"
+  },
+  {
+    "code": "KSF",
+    "label": "Kassel"
+  },
+  {
+    "code": "BER",
+    "label": "Berlin"
+  },
+  {
+    "code": "BSL",
+    "label": "Basel"
+  },
+  {
+    "code": "FMM",
+    "label": "Memmingen"
+  },
+  {
+    "code": "FDH",
+    "label": "Friedrichshafen"
+  },
+  {
+    "code": "BRE",
+    "label": "Bremen"
+  },
+  {
+    "code": "NRN",
+    "label": "Weeze"
+  },
+  {
+    "code": "ZRH",
+    "label": "Zurich"
+  },
+  {
+    "code": "PAD",
+    "label": "Paderborn"
+  },
+  {
+    "code": "ERF",
+    "label": "Erfurt"
+  },
+  {
+    "code": "LBC",
+    "label": "Luebeck"
+  },
+  {
+    "code": "PRG",
+    "label": "Prague"
+  },
+  {
+    "code": "VIE",
+    "label": "Vienna"
+  },
+  {
+    "code": "SZG",
+    "label": "Salzburg"
+  },
+  {
+    "code": "EIN",
+    "label": "Eindhoven"
+  },
+  {
+    "code": "AMS",
+    "label": "Amsterdam"
+  },
+  {
+    "code": "INN",
+    "label": "Innsbruck"
+  },
+  {
+    "code": "LUX",
+    "label": "Luxemburg"
+  },
+  {
+    "code": "BLL",
+    "label": "Billund"
+  },
+  {
+    "code": "LNZ",
+    "label": "Linz"
+  },
+  {
+    "code": "GVA",
+    "label": "Geneva"
+  },
+  {
+    "code": "SXB",
+    "label": "Strasbourg"
+  },
+  {
+    "code": "KRK",
+    "label": "Krakow"
+  },
+  {
+    "code": "BRU",
+    "label": "Brussels"
+  },
+  {
+    "code": "WAW",
+    "label": "Warsaw"
+  },
+  {
+    "code": "GRZ",
+    "label": "Graz"
+  },
+  {
+    "code": "GRQ",
+    "label": "Groningen"
+  },
+  {
+    "code": "KLU",
+    "label": "Klagenfurt"
+  },
+  {
+    "code": "GWT",
+    "label": "Westerland"
+  },
+  {
+    "code": "BRN",
+    "label": "Bern"
+  },
+  {
+    "code": "BRS",
+    "label": "Bristol"
+  }
+]
